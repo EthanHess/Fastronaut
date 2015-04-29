d8e0985f00b360c100b70de66356c4316a3b011e
Ethan Hess <ethanhess@Ethans-MacBook-Pro.local>
2015-04-29 16:53:30 -0600

    second level code added

--- FastronautGame/SecondLevelViewController.m
+++ FastronautGame/SecondLevelViewController.m
@@ -25,6 +25,111 @@
 
 - (IBAction)startGame:(id)sender {
     
+    self.startGame.hidden = YES;
+    
+    self.scubaTimer = [NSTimer timerWithTimeInterval:0.05 target:self selector:@selector(scubaMoving) userInfo:nil repeats:YES];
+    
+    [self placeObstacles];
+    
+    self.obstacleTimer = [NSTimer timerWithTimeInterval:0.005 target:self selector:@selector(obstaclesMoving) userInfo:nil repeats:YES];
+    
+}
+
+- (void)obstaclesMoving {
+    
+    self.piranha.center = CGPointMake(self.piranha.center.x - 1, self.piranha.center.y);
+    
+    if (self.piranha.center.x < - 35) {
+        [self placeObstacles];
+    }
+    
+    if (self.piranha.center.x == 30) {
+        [self score];
+    }
+    
+    if (CGRectIntersectsRect(self.scubanaut.frame, self.piranha.frame)) {
+        [self gameEnded];
+    }
+    
+    if (self.scubanaut.center.y > self.view.frame.size.height - self.scubanaut.frame.size.height / 2) {
+        [self gameEnded]; 
+    }
+    
+    if (self.scubanaut.center.y < 0 + self.scubanaut.frame.size.height / 2) {
+        [self gameEnded];
+    }
+    
+    
+}
+
+- (void)placeObstacles {
+    
+    int frame = self.view.frame.size.height;
+    
+    obstaclePostition = arc4random() %frame;
+    
+    self.piranha.center = CGPointMake(380, obstaclePostition);
+    
+    if (self.piranha.center.y < 200) {
+        self.piranha.image = [UIImage imageNamed:@"seagull"]; 
+    }
+    
+    
+}
+
+- (void)scubaMoving {
+    
+    self.scubanaut.center = CGPointMake(self.scubanaut.center.x, self.scubanaut.center.y - scubaFlight);
+    
+    scubaFlight = scubaFlight - 5;
+    
+    if (scubaFlight < - 15) {
+        scubaFlight = -15;
+    }
+    
+    if (scubaFlight > 0) {
+        self.scubanaut.image = [UIImage imageNamed:@"swimUp"];
+    }
+    
+    if (scubaFlight < 0) {
+        self.scubanaut.image = [UIImage imageNamed:@"swimDown"];
+    }
+    
+    
+    
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    scubaFlight = 30;
+    
+}
+
+- (void)gameEnded {
+    
+    [self.scubaTimer invalidate];
+    [self.obstacleTimer invalidate];
+    
+    self.scubanaut.hidden = YES;
+    self.gameOver.hidden = NO;
+    self.piranha.hidden = YES; 
+    
+}
+
+- (void)score {
+    
+    scoreNumber = scoreNumber + 1;
+    
+    if (scoreNumber > 10) {
+        [self.scubaTimer invalidate];
+        [self.obstacleTimer invalidate];
+        
+        self.proceedButton.hidden = NO;
+        self.scubanaut.hidden = YES;
+        self.piranha.hidden = YES; 
+        
+    }
+    
     
 }
 
