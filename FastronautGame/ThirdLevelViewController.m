1907b8e328fd5f654b1e53369208b625a8d3d63d
Ethan Hess <ethanhess@Ethans-MacBook-Pro.local>
2015-04-30 14:14:18 -0600

    third level complete

--- FastronautGame/ThirdLevelViewController.m
+++ FastronautGame/ThirdLevelViewController.m
@@ -38,35 +38,103 @@
 
 - (void)fastroMoving {
     
+    self.fastronaut.center = CGPointMake(self.fastronaut.center.x, self.fastronaut.center.y - astroFlight);
+    
+    astroFlight = astroFlight - 5; 
+    
+    if (astroFlight < -15) {
+        astroFlight = -15;
+    }
+    
+    if (astroFlight > 0) {
+        self.fastronaut.image = [UIImage imageNamed:@"cityUp"];
+    }
+    
+    if (astroFlight < 0) {
+        self.fastronaut.image = [UIImage imageNamed:@"cityDown"];
+    }
     
 }
 
 - (void)copterMoving {
+    
+    self.helicopter.center = CGPointMake(self.helicopter.center.x - 1, self.helicopter.center.y); 
+    
+    if (self.helicopter.center.x < -35) {
+        [self placeCopter];
+    }
+    
+    if (self.helicopter.center.x == 30) {
+        [self score];
+    }
+    
+    if (CGRectIntersectsRect(self.fastronaut.frame, self.helicopter.frame)) {
+        [self gameEnded];
+    }
+    
+    if (self.fastronaut.center.y > self.view.frame.size.height - self.fastronaut.frame.size.height / 2) {
+        [self gameEnded];
+    }
+    
+    if (self.fastronaut.center.y < 0 + self.fastronaut.frame.size.height / 2) {
+        [self gameEnded]; 
+    }
     
     
 }
 
 - (void)placeCopter {
     
+    int frame = self.view.frame.size.height;
+    
+    copterPostition = arc4random() %frame;
+    
+    self.helicopter.center = CGPointMake(380, copterPostition);
+    
+    //future if statement to change image if score is higher than certain number;
     
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     
+    astroFlight = 30;
     
 }
 
 - (void)score {
     
+    scoreNumber = scoreNumber + 1;
+    
+    if (scoreNumber > 5) {
+        [self.fastroTimer invalidate];
+        [self.copterTimer invalidate];
+        
+        self.proceedButton.hidden = NO;
+        self.fastronaut.hidden = YES;
+        self.helicopter.hidden = YES;
+        
+    }
     
 }
 
 - (void)gameEnded {
     
+    [self.fastroTimer invalidate];
+    [self.copterTimer invalidate];
+    
+    self.fastronaut.hidden = YES;
+    self.tryAgain.hidden = NO;
+    self.helicopter.hidden = YES;
+    
     
 }
 
 - (IBAction)restartGame:(id)sender {
+    
+    self.startGame.hidden = NO;
+    self.fastronaut.hidden = NO;
+    self.helicopter.hidden = NO;
+    self.tryAgain.hidden = YES; 
     
     
 }
