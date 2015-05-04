2966fc2915138de141d378fef5f736fd3a14c226
Ethan Hess <ethanhess@slkc.firstdigital.com>
2015-05-03 19:38:32 -0600

    Castle view added

---
+++ FastronautGame/SecondLevelView.h
@@ -0,0 +1,13 @@
+//
+//  SecondLevelView.h
+//  FastronautGame
+//
+//  Created by Ethan Hess on 5/3/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondLevelView : UIViewController
+
+@end
---
+++ FastronautGame/SecondLevelView.m
@@ -0,0 +1,29 @@
+//
+//  SecondLevelView.m
+//  FastronautGame
+//
+//  Created by Ethan Hess on 5/3/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "SecondLevelView.h"
+
+@interface SecondLevelView ()
+
+@end
+
+@implementation SecondLevelView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
